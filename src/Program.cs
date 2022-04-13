763f0a83a9d6d667170f5f9bed6fb202edc569cb
488Ques <abeces968@gmail.com>
2022-04-13 21:42:17 +0700

    Update example in Main

--- src/Program.cs
+++ src/Program.cs
@@ -6,20 +6,18 @@ public class Program
     {
         Apartment ap = new Apartment("ABC", "ABC 123");
 
-        ap.RoomTable.Add(32, 3);
+        ap.RoomTable.Add(0100, 32, 3);
+
         Console.WriteLine("Phòng trống khi chưa đăng ký người ở");
-        Console.WriteLine(ap.RoomTable.Get(0));
+        Console.WriteLine(ap.RoomTable.GetByRoomID(100));
 
-        ap.TenantTable.Add("Fag", 2, true, "242141241", 0);
-        ap.TenantTable.Add("Got", 3, false, "79199922", 0);
-
-        // foreach (Tenant t in ap.TenantTable.All())
-        // {
-        //     Console.WriteLine(t);
-        // }
+        ap.TenantTable.Add("A", 20, true, "321987654321", 0100);
+        ap.TenantTable.Add("B", 19, false, "123456789123", 0100);
 
         Console.WriteLine("Phòng có 2 người sau khi đăng ký người ở");
         Console.WriteLine(ap.RoomTable.Get(0));
 
+        // Register tenant to invalid room
+        // ap.TenantTable.Add("C", 41, false, "123456789012", 2);
     }
 }
