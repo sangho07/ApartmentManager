99415d7c1efa1882578247afc195677850cb16a6
488Ques <abeces968@gmail.com>
2022-04-13 21:17:52 +0700

    Add GetByRoomID

--- src/data/RoomTable.cs
+++ src/data/RoomTable.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+
 public class RoomTable : ATable
 {
     public override void Update(int id)
@@ -5,8 +8,17 @@ public class RoomTable : ATable
         throw new System.NotImplementedException();
     }
 
-    public void Add(int price, int maxTenants)
+    public Room GetByRoomID(int roomID)
     {
+        var r = records.Find(rec =>
+        {
+            Room room = (Room)rec;
+            return room.roomID == roomID;
+        });
+        if (r == null) throw new ArgumentException("No such roomID exists");
+        return (Room)r;
+    }
+
     public void Add(int roomID, int price, int maxTenants)
     {
         base.Add(new Room(roomID, price, maxTenants));
