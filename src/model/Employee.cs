c8d9bd8e143bf0d171920a5ce4201e6ecb6b8a9c
phamviettung2002 <phamviettung2002@gmail.com>
2022-04-16 12:53:00 +0700

    sữa person và employee

--- src/model/Employee.cs
+++ src/model/Employee.cs
@@ -2,12 +2,24 @@ public class Employee : Person
 {
     private int salary;
     private string role;
+    private int workday;
 
-    public Employee(string name, int age, bool gender, string ssn, int salary, string role) : base(name, age, gender, ssn)
+
+    public Employee(string name, int age, bool gender, string ssn, int workday, string role) : base(name, age, gender, ssn)
     {
-        this.salary = salary;
+        this.workday = workday;
+        this.salary = workday * 20;
         this.role = role;
     }
+    public void UpdateRole(string role)
+    {
+        this.role = role;
+    }
+    public void UpdateSalary(int workday)
+    {
+        this.salary = workday * 20;
+    }
+
 
     public override string ToString()
     {
--- src/model/Person.cs
+++ src/model/Person.cs
@@ -7,13 +7,18 @@ public class Person : IRecord
     private bool gender; // true = Male, false = Female
     private string ssn; // Unique
 
+
     public Person(string name, int age, bool gender, string ssn)
     {
         this.name = name;
         this.age = age;
         this.gender = gender;
-        this.ssn = ssn;
+        if (ssn.Length == 12)
+        {
+            this.ssn = ssn;
+        }
     }
+
 
     public override string ToString()
     {
