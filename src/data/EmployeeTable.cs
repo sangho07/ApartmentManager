e108758fb1194e89ba315b90a33f4fee0fe23643
phamviettung2002 <phamviettung2002@gmail.com>
2022-04-16 12:50:36 +0700

    Sữa emplyeetable

--- src/data/EmployeeTable.cs
+++ src/data/EmployeeTable.cs
@@ -1,7 +1,19 @@
 public class EmployeeTable : Table
 {
-    public void Add(string name, int age, bool gender, string ssn, int salary, string role)
+    public void Add(string name, int age, bool gender, string ssn, int workday, string role)
     {
+        int salary = workday * 20;
         base.Add(new Employee(name, age, gender, ssn, salary, role));
     }
+    public void UpdateRole(int id, string role)
+    {
+        Employee e = (Employee)base.Get(id);
+        e.UpdateRole(role);
+    }
+    public void UpdateSalary(int id, int workday)
+    {
+        Employee e = (Employee)base.Get(id);
+        e.UpdateSalary(workday);
+    }
+
 }
